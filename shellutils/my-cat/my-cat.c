5e2ff0f450bfd9b201ede58096e169650f7a37ee
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2021-04-19 14:36:20 +0300

    my-cat

---
+++ shellutils/my-cat/my-cat.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    FILE *file = NULL;
+    char buffer[BUFSIZ];
+    if (argc <= 1){
+       exit(0);
+    }
+
+    for (size_t i = 1; i < argc; i++){
+        if((file = fopen(argv[i], "r")) == NULL){
+            printf("my-cat: cannot open file\n");
+            exit(1);
+        }
+
+        while(fgets(buffer, BUFSIZ, file)){
+            printf("%s", buffer); 
+        }
+        
+        fclose(file);    
+    }
+
+    
+}
