39bc27dca449722c366441dcab237c438e794c9d
Pyry-Santahuhta <61690808+Pyry-Santahuhta@users.noreply.github.com>
2021-04-21 14:51:46 +0300

    Unzip

--- shellutils/my-zip/my-unzip.c
+++ shellutils/my-zip/my-unzip.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define segmentsize 5;
-
 
 int main(int argc, char *argv[]){
     FILE *file = NULL;
-    char c[2], previous_c[2];
+    char c[1];
     int count = 0;
 
     if (argc <= 1){
@@ -16,29 +14,17 @@ int main(int argc, char *argv[]){
 
     for (size_t i = 1; i < argc; i++){
         if((file = fopen(argv[i], "r")) == NULL){
-            printf("my-cat: cannot open file\n");
+            printf("my-unzip: cannot open file\n");
             exit(1);
         }
 
-        while(fread(c, 1, 1, file)){
-            if(feof(file)){
-                break;
+        while(fread(&count, 4, 1, file)){
+            fread(&c, 1, 1, file);
+            for (size_t j = 0; j < count; j++){
+                printf("%s", c);
             }
-
-            if(strcmp(c, previous_c) == 0){
-                count++;
-            }else{
-                if(previous_c != "\0"){
-                    fwrite(&count, 4, 1, stdout);
-                    fwrite(previous_c, 1, 1, stdout);
-                }
-                count = 1;
-                strcpy(previous_c, c);
-
-            } 
-        }
+        } 
         fclose(file);    
     }
-
-    
+    return 0;
 }
